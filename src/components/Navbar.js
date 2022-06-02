8b0cb2a03f1a06d7aadcfc037a992b16a6d20f0c
alfaizsry <2010511071@mahasiswa.upnvj.ac.id>
2022-06-02 14:34:13 +0700

    minor update on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <div className="bg-dark absolute top-0 left-0 w-full flex items-center z-10">
-      <div className="container">
+    <nav className="bg-dark absolute border-white px-2 sm:px-4 py-2.5 rounded dark:bg-white top-0 left-0 w-full flex items-center z-10">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
         <div className="flex items-center justify-between relative">
-          <div className="px-4">
-            <a href="#" className="py-6 block">
-              <img src={logo} alt="" />
+          <div className="px-2">
+            <a href="#" className="py-1 block">
+              <img className="scale-75" src={logo} alt="" />
             </a>
           </div>
           <div className="flex items-center px-4 bg-dark">
@@ -25,7 +25,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
