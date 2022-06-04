e962629da5425b8ef8e348746284379ba2fab1cf
alfaizsry <2010511071@mahasiswa.upnvj.ac.id>
2022-06-04 16:19:17 +0700

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,78 @@
 import React, { useState } from 'react';
 import logo from '../assets/logo.png';
-
+import profileLogo from '../assets/dummy-profile.png';
 const Navbar = () => {
   return (
-    <nav className="bg-dark absolute border-white px-2 sm:px-4 py-2.5 rounded dark:bg-white top-0 left-0 w-full flex items-center z-10">
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <div className="flex items-center justify-between relative">
-          <div className="px-2">
-            <a href="#" className="py-1 block">
-              <img className="scale-75" src={logo} alt="" />
+        <a href="#" className="py-1 block">
+          <img className="scale-75" src={logo} alt="" />
+        </a>
+        <div className="flex bg" id="hamburger-mobile">
+          <button
+            data-collapse-toggle="mobile-menu"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mobile-menu"
+            aria-expanded="false"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <svg
+              className="hidden w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className="hidden justify-between items-center w-full md:flex md:w-auto md:mt-0.5"
+          id="user-profile"
+        >
+          <div className="px-3">
+            <a
+              href="#"
+              className="block py-2 pr-3 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              aria-current="page"
+            >
+              Sell
             </a>
           </div>
-          <div className="flex items-center px-4 bg-dark">
-            <button
-              id="hamburger"
-              name="humberger"
-              type="button"
-              className="block absolute right-4"
+          <div className="px-3">
+            <a
+              href="#"
+              className="block py-2 pr-3 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
             >
-              <span className="w-[30px] h-[2px] my-2 block;"></span>
-              <span className="w-[30px] h-[2px] my-2 block;"></span>
-              <span className="w-[30px] h-[2px] my-2 block;"></span>
-            </button>
+              Category
+            </a>
           </div>
+          <button
+            type="button"
+            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="dropdown"
+          >
+            <span className="sr-only">Open user menu</span>
+            <image className="w-8 h-8 rounded-full" src={profileLogo} alt="user photo" />
+          </button>
         </div>
       </div>
     </nav>
